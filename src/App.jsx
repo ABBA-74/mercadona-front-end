8f3b90f12d2fb738e0232e3bc3d7ea19de1538b4
Abdel Bailoul <ab.bailoul@gmail.com>
2023-10-03 20:06:57 +0200

    feat: added routes for app navigation based on navbar links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
+import HomePage from './pages/Home/HomePage';
+import PromotionsPage from './pages/Promotions/PromotionsPage';
+import CatalogPage from './pages/Catalog/CatalogPage';
+import WishListPage from './pages/WishList/WishListPage';
+import LoginPage from './pages/Login/LoginPage';
 
 function App() {
   return (
     <>
       <Navigation />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/promotions' element={<PromotionsPage />} />
+        <Route path='/catalogue' element={<CatalogPage />} />
+        <Route path='/liste-favoris' element={<WishListPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
     </>
   );
 }
