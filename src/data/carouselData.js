1cb7c8c82661ebc82c9b3a03294450c61897571e
Abdel Bailoul <ab.bailoul@gmail.com>
2023-10-04 01:18:07 +0200

    feat: added carousel component + updated style navbar

---
+++ src/data/carouselData.js
@@ -0,0 +1,26 @@
+export const carouselItemsList = [
+  {
+    id: 1,
+    image: {
+      label: 'kinder bueno',
+      imgFile: 'kinder-bueno.webp',
+    },
+    discount: -50,
+  },
+  {
+    id: 2,
+    image: {
+      label: "céréales kellogg's",
+      imgFile: 'cereales-kelloggs.webp',
+    },
+    discount: -75,
+  },
+  {
+    id: 3,
+    image: {
+      label: "café l'or",
+      imgFile: 'cafe-l-or.webp',
+    },
+    discount: -65,
+  },
+];
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Container } from 'react-bootstrap';
 import './HomePage.scss';
+import CarouselHeader from '../../components/Carousel/CarouselHeader';
 
 const HomePage = () => {
   return (
     <>
+      <CarouselHeader />
       <Container>
         <h1>Home Page</h1>
       </Container>
