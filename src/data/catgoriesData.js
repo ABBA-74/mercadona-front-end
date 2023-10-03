a63accec2eba5d12e0b1c0e8e5a4e056676acded
Abdel Bailoul <ab.bailoul@gmail.com>
2023-10-03 18:38:32 +0200

    feat: import Quicksand and Poppins fonts in index.css

---
+++ src/data/catgoriesData.js
@@ -0,0 +1,98 @@
+export const categoriesList = [
+  {
+    id: 13,
+    label: 'Aperitif',
+    image: {
+      label: 'Aperitif',
+      imgFile: 'aperitif.png',
+    },
+  },
+  {
+    id: 14,
+    label: 'Boucherie',
+    image: {
+      label: 'Boucherie',
+      imgFile: 'boucherie.png',
+    },
+  },
+  {
+    id: 15,
+    label: 'Dessert',
+    image: {
+      label: 'Dessert',
+      imgFile: 'dessert.png',
+    },
+  },
+  {
+    id: 16,
+    label: 'Fromages',
+    image: {
+      label: 'Fromages',
+      imgFile: 'fromages.png',
+    },
+  },
+  {
+    id: 17,
+    label: 'Fruits et legumes bio',
+    image: {
+      label: 'Fruits et legumes bio',
+      imgFile: 'fruits-legumes-bio.png',
+    },
+  },
+  {
+    id: 18,
+    label: 'Fruits',
+    image: {
+      label: 'Fruits',
+      imgFile: 'fruits.png',
+    },
+  },
+  {
+    id: 19,
+    label: 'Glaces',
+    image: {
+      label: 'Glaces',
+      imgFile: 'glaces.png',
+    },
+  },
+  {
+    id: 20,
+    label: 'Jus de fruits frais',
+    image: {
+      label: 'Jus de fruits frais',
+      imgFile: 'jus-fruits-frais.png',
+    },
+  },
+  {
+    id: 21,
+    label: 'Legumes',
+    image: {
+      label: 'Legumes',
+      imgFile: 'legumes.png',
+    },
+  },
+  {
+    id: 22,
+    label: 'Pates et riz',
+    image: {
+      label: 'Pates et riz',
+      imgFile: 'pates-riz.png',
+    },
+  },
+  {
+    id: 23,
+    label: 'Poissons et crustaces',
+    image: {
+      label: 'Poissons et crustaces',
+      imgFile: 'poissons-crustaces.png',
+    },
+  },
+  {
+    id: 24,
+    label: 'Volaille et lapin',
+    image: {
+      label: 'Volaille et lapin',
+      imgFile: 'volaille-lapin.png',
+    },
+  },
+];
