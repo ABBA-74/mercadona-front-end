23ee66adfcfdd14d9359928cd432bed1a17122bc
Abdel Bailoul <ab.bailoul@gmail.com>
2023-10-04 09:30:05 +0200

    fix: resolved navbar brand click issue

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -13,18 +13,35 @@ import searchIcon from '../../assets/icons/search-menu.png';
 import mercadonaLogo from '../../assets/images/mercadona-logo.svg';
 import './Navigation.scss';
 import { Link } from 'react-router-dom';
+import { useRef } from 'react';
 
 const Navigation = () => {
+  const refPromotionsNavItem = useRef(null);
+  const refCatalogNavItem = useRef(null);
+  const refWishListNavItem = useRef(null);
+  const refLoginNavItem = useRef(null);
+
   const navDropdownTitle = (
     <>
       <img
         src={categoryListIcon}
-        alt=''
+        alt='icone catégorie'
         className='menu-icon category-list-icon'
       />
       <span className='label-menu'>Rayons</span>
     </>
   );
+
+  const handleClickNavbarBrand = () => {
+    [
+      refPromotionsNavItem,
+      refCatalogNavItem,
+      refWishListNavItem,
+      refLoginNavItem,
+    ].forEach((el) => {
+      el.current.classList.remove('active');
+    });
+  };
 
   return (
     <>
@@ -35,9 +52,21 @@ const Navigation = () => {
         className='bg-body-tertiary'
       >
         <Container fluid className='mx-3 mx-md-5'>
-          <Navbar.Brand href='/'>
-            <img src={mercadonaLogo} alt='' className='menu-icon search-icon' />
-            <span className='brand-name'>Mercadona</span>
+          <Navbar.Brand>
+            <Link
+              as={Link}
+              eventkey='1'
+              to='/'
+              className='d-flex'
+              onClick={handleClickNavbarBrand}
+            >
+              <img
+                src={mercadonaLogo}
+                alt='Mercadona Logo'
+                className='menu-icon search-icon'
+              />
+              <span className='brand-name'>Mercadona</span>
+            </Link>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav' className='w-100'>
@@ -49,7 +78,11 @@ const Navigation = () => {
                   aria-describedby='basic-addon2'
                 />
                 <InputGroup.Text id='basic-addon2'>
-                  <img src={searchIcon} alt='' className='search-icon' />
+                  <img
+                    src={searchIcon}
+                    alt='icone loupe'
+                    className='search-icon'
+                  />
                 </InputGroup.Text>
               </InputGroup>
               <NavDropdown
@@ -90,50 +123,58 @@ const Navigation = () => {
               </NavDropdown>
               <Nav.Link
                 as={Link}
-                eventKey='1'
-                className='nav-link mx-lg-2'
+                eventKey='2'
+                className='mx-lg-2'
                 to='/promotions'
+                ref={refPromotionsNavItem}
               >
                 <img
                   src={promotionsIcon}
-                  alt=''
+                  alt='icone promotions'
                   className='menu-icon promotions-icon'
                 />
                 <span className='label-menu'>Promotions</span>
               </Nav.Link>
               <Nav.Link
-                eventKey='2'
+                eventKey='3'
                 as={Link}
-                className='nav-link mx-lg-2'
+                className='mx-lg-2'
                 to='/catalogue'
+                ref={refCatalogNavItem}
               >
                 <img
                   src={catalogIcon}
-                  alt=''
+                  alt='icone catalogue'
                   className='menu-icon catalog-icon catalog-icon'
                 />
                 <span className='label-menu'>Catalogue</span>
               </Nav.Link>
               <Nav.Link
-                eventKey='3'
+                eventKey='4'
                 as={Link}
-                className='nav-link mx-lg-2'
+                className='mx-lg-2'
                 to='/liste-favoris'
+                ref={refWishListNavItem}
               >
                 <img
                   src={shoppingWishListIcon}
-                  alt=''
+                  alt='icone liste des souhaits'
                   className='menu-icon shopping-wish-list-icon'
                 />
                 <span className='label-menu'>Mes produits</span>
               </Nav.Link>
               <Nav.Link
-                eventKey='4'
+                eventKey='5'
                 as={Link}
-                className='nav-link ms-lg-2'
+                className='ms-lg-2'
                 to='/login'
+                ref={refLoginNavItem}
               >
-                <img src={loginIcon} alt='' className='menu-icon login-icon' />
+                <img
+                  src={loginIcon}
+                  alt='icone login'
+                  className='menu-icon login-icon'
+                />
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
