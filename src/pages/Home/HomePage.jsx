54a02e40c73722ae8b42f224d590d89be78890c5
Abdel Bailoul <ab.bailoul@gmail.com>
2023-10-04 09:42:55 +0200

    feat: added CarouselCategories and styled homepage layout

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,14 +1,20 @@
 import { Container } from 'react-bootstrap';
 import './HomePage.scss';
 import CarouselHeader from '../../components/Carousel/CarouselHeader';
+import CarouselCategories from '../../components/CarouselCategories/CarouselCategories';
 
 const HomePage = () => {
   return (
     <>
       <CarouselHeader />
-      <Container>
-        <h1>Home Page</h1>
-      </Container>
+      <main className='main-homepage'>
+        <Container>
+          <h1>
+            Ne cherchez plus, vos produits préférés sont là en permanence !
+          </h1>
+          <CarouselCategories />
+        </Container>
+      </main>
     </>
   );
 };
