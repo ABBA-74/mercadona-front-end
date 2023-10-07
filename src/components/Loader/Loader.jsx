702737117bb1408c5e1e0096e13418713d3fc1d5
Abdel Bailoul <ab.bailoul@gmail.com>
2023-10-08 00:26:52 +0200

    feat: created Loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,17 @@
+import './Loader.scss';
+
+const Loader = () => {
+  return (
+    <div className='spinner-wrapper'>
+      <div className='spinner'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
