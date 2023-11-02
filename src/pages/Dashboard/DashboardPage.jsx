00362d3559a96033ac29ee141c082f025be00a90
Abdel Bailoul <ab.bailoul@gmail.com>
2023-11-02 22:40:44 +0100

    feat: updated layout of dahsboard page

--- src/pages/Dashboard/DashboardPage.jsx
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -1,15 +1,25 @@
-import { Container } from 'react-bootstrap';
+import { useEffect } from 'react';
 import './DashboardPage.scss';
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar/Sidebar';
+import TopBar from './TopBar/TopBar';
+import { scrollTo } from '../../utils/scrollTo';
 
 const DashboardPage = () => {
+  useEffect(() => {
+    scrollTo(0, 0);
+  }, []);
+
   return (
-    <>
-      <section className='section-dashboard'>
-        <Container>
-          <h1>Dashboard Page</h1>
-        </Container>
-      </section>
-    </>
+    <main className='dashboard'>
+      <TopBar />
+      <div className='container-dashboard'>
+        <Sidebar />
+        <div className='dashboard-content'>
+          <Outlet />
+        </div>
+      </div>
+    </main>
   );
 };
 
