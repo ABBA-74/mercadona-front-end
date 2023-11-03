ca63bd97308144138963eeb7a1f4de749b62c5fe
Abdel Bailoul <ab.bailoul@gmail.com>
2023-11-03 01:46:28 +0100

    feat: added skeleton React components and SCSS structures for Products

---
+++ src/pages/Dashboard/Products/ProductList.jsx
@@ -0,0 +1,11 @@
+import './ProductList.scss';
+
+const ProductList = () => {
+  return (
+    <section className='section-products'>
+      <h3>Produits</h3>
+    </section>
+  );
+};
+
+export default ProductList;
