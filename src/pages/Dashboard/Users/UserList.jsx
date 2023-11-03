6acf23f20c4653b184d4b8ef95003523d1251df2
Abdel Bailoul <ab.bailoul@gmail.com>
2023-11-03 01:47:34 +0100

    feat: added skeleton React components and SCSS structures for Users

---
+++ src/pages/Dashboard/Users/UserList.jsx
@@ -0,0 +1,11 @@
+import './UserList.scss';
+
+const UserList = () => {
+  return (
+    <section className='section-user-list'>
+      <h3>UserList</h3>
+    </section>
+  );
+};
+
+export default UserList;
