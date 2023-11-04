23712155b2b8fe129fc137e5bde8944b858079e4
Abdel Bailoul <ab.bailoul@gmail.com>
2023-11-04 09:31:02 +0100

    feat: implemented interactive product list using MUI DataGrid component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,10 +1,10 @@
-import { v4 as uuidv4 } from 'uuid';
 import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
 import { Badge } from 'react-bootstrap';
 import { BsStarFill, BsStar } from 'react-icons/bs';
 import { MdOutlineFavoriteBorder } from 'react-icons/md';
-// import { MdOutlineFavorite } from 'react-icons/md';
 import redPennant from '../../assets/images/red-pennant.svg';
+import { API_URL_IMG } from '../../api/apiConfig';
 import './ProductCard.scss';
 
 const ProductCard = ({
@@ -19,7 +19,7 @@ const ProductCard = ({
 }) => {
   const filledStars = Math.floor(Math.random() * 3) + 3;
   const rndReviews = Math.floor(Math.random() * 50) + 9;
-  const imageUrl = `https://mercadona-api.abb-dev.fr/images/${imgFile}`;
+  const imageUrl = `${API_URL_IMG}/${imgFile}`;
   const discountedPriceFormatted = String(discountedPrice?.toFixed(2)).replace(
     '.',
     '€'
@@ -30,58 +30,56 @@ const ProductCard = ({
   );
 
   return (
-    <>
-      <div className='product-card'>
-        {discountPercentage && (
-          <div className='product-card__label-discount'>
-            <img src={redPennant} alt='fanion discount' />
-            <span>{discountPercentage}%</span>
-          </div>
-        )}
-        <div className='product-card__top'>
-          <div className='product-card__img-wrapper'>
-            <img src={imageUrl} alt={imgAlt} />
-          </div>
-          <Badge className='mt-3' bg='secondary'>
-            {category}
-          </Badge>
+    <div className='product-card'>
+      {discountPercentage && (
+        <div className='product-card__label-discount'>
+          <img src={redPennant} alt='fanion discount' />
+          <span>{discountPercentage}%</span>
         </div>
-        <div className='product-card__content'>
-          <div className='product-card__price'>
-            {discountPercentage ? (
-              <>
-                <span className='total-price'>{discountedPriceFormatted}</span>
-                <span className='ms-3 previous-price'>
-                  {originalPriceFormatted}
-                </span>
-              </>
-            ) : (
-              <span className='total-price'>{originalPriceFormatted}</span>
-            )}
-          </div>
-          <div className='product-card__reviews'>
-            <ul className='star-list'>
-              {[...Array(5)].map((_, i) => (
-                <li key={uuidv4()}>
-                  {i < filledStars ? (
-                    <BsStarFill color='orange' />
-                  ) : (
-                    <BsStar color='orange' />
-                  )}
-                </li>
-              ))}
-              <li>&nbsp;&nbsp;{rndReviews}</li>
-            </ul>
-          </div>
-          <p className='product-card__label'>{label}</p>
-          <p className='product-card__description'>{description}</p>
-          <button className='btn btn-primary'>
-            <MdOutlineFavoriteBorder />
-            &nbsp; Ajouter à ma liste
-          </button>
+      )}
+      <div className='product-card__top'>
+        <div className='product-card__img-wrapper'>
+          <img src={imageUrl} alt={imgAlt} />
         </div>
+        <Badge className='mt-3' bg='secondary'>
+          {category}
+        </Badge>
       </div>
-    </>
+      <div className='product-card__content'>
+        <div className='product-card__price'>
+          {discountPercentage ? (
+            <>
+              <span className='total-price'>{discountedPriceFormatted}</span>
+              <span className='ms-3 previous-price'>
+                {originalPriceFormatted}
+              </span>
+            </>
+          ) : (
+            <span className='total-price'>{originalPriceFormatted}</span>
+          )}
+        </div>
+        <div className='product-card__reviews'>
+          <ul className='star-list'>
+            {[...Array(5)].map((_, i) => (
+              <li key={uuidv4()}>
+                {i < filledStars ? (
+                  <BsStarFill color='orange' />
+                ) : (
+                  <BsStar color='orange' />
+                )}
+              </li>
+            ))}
+            <li>&nbsp;&nbsp;{rndReviews}</li>
+          </ul>
+        </div>
+        <p className='product-card__label'>{label}</p>
+        <p className='product-card__description'>{description}</p>
+        <button className='btn btn-primary'>
+          <MdOutlineFavoriteBorder />
+          &nbsp; Ajouter à ma liste
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/pages/Dashboard/Products/ProductList.jsx
+++ src/pages/Dashboard/Products/ProductList.jsx
@@ -1,9 +1,122 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { DataGrid, frFR } from '@mui/x-data-grid';
+import { Box, ThemeProvider } from '@mui/material';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import ErrorMessage from '../../../components/ErrorMessage/ErrorMessage';
+import useWidthCheck from '../../../hooks/useWidthCheck';
+import theme from '../theme/dataGridTheme';
+import { getProducts } from '../../../api/getProducts';
+import { fetchErrorMessage } from '../../../data/errorMessages';
 import './ProductList.scss';
+import columns from './config/productGridColumns';
 
 const ProductList = () => {
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const [error, setError] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [pageSize, setPageSize] = useState(8);
+  const [rowCount, setRowCount] = useState(8);
+  const [rowCountState, setRowCountState] = useState(null);
+  const [paginationModel, setPaginationModel] = useState({
+    pageSize: 8,
+    page: 0,
+  });
+  const isWidthAdaptForTable = useWidthCheck(1024);
+
+  const fetchProducts = async () => {
+    try {
+      setIsLoading(true);
+      const { products, totalItems } = await getProducts(page, null, true);
+      setData(products);
+      setRowCount(totalItems);
+      setError(null);
+    } catch (err) {
+      console.error('Erreur lors de la récupération des données', error);
+      setError(err);
+      if (err.response && err.response.data.code === 401) {
+        // Utilisez navigate pour rediriger impérativement
+        navigate('/login', { replace: true });
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handlePageChange = (model) => {
+    setPaginationModel(model);
+    setPage(model.page + 1);
+  };
+
+  useEffect(() => {
+    setRowCountState((prevRowCountState) =>
+      rowCount !== undefined ? rowCount : prevRowCountState
+    );
+  }, [rowCount, setRowCountState]);
+
+  /* Fetch Data (products) on mounted phase of the component */
+  useEffect(() => {
+    fetchProducts();
+  }, [page, pageSize]);
+
   return (
     <section className='section-products'>
-      <h3>Produits</h3>
+      {error && (
+        <section className='section-error-fetch-msg'>
+          <ErrorMessage
+            title={fetchErrorMessage.title}
+            message={fetchErrorMessage.message}
+          />
+          ;
+        </section>
+      )}
+      {data?.length > 0 && isWidthAdaptForTable && (
+        <>
+          <div className='section-list-products-top-container mb-4'>
+            <h3 className='h5'>Liste des produits</h3>
+            <Button>
+              <AddCircleIcon className='icon-btn-create-item me-2' />
+              Créer un produit
+            </Button>
+          </div>
+          <div className='section-list-products-table-container'>
+            <ThemeProvider theme={theme}>
+              <Box sx={{ height: 547, minWidth: '0', overflowX: 'none' }}>
+                <DataGrid
+                  className='section-list-products-table'
+                  horizontalScrollBarVisibility='Auto'
+                  rows={data}
+                  columns={columns}
+                  loading={isLoading}
+                  localeText={
+                    frFR.components.MuiDataGrid.defaultProps.localeText
+                  }
+                  disableRowSelectionOnClick
+                  paginationMode='server'
+                  rowCount={rowCountState}
+                  pageSize={pageSize}
+                  pageSizeOptions={[8]}
+                  page={paginationModel.page}
+                  paginationModel={paginationModel}
+                  onPaginationModelChange={handlePageChange}
+                />
+              </Box>
+            </ThemeProvider>
+          </div>
+        </>
+      )}
+      {!isLoading && data?.length > 0 && !isWidthAdaptForTable && (
+        <>
+          <h3 className='h5 mb-4'>Ajustement d&apos;écran recommandé</h3>
+          <p>
+            Pour une expérience optimale, veuillez utiliser un écran plus grand.
+            Nous vous remercions pour votre compréhension.
+          </p>
+        </>
+      )}
     </section>
   );
 };
