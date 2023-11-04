9e9b7f5987a7b8013f18220daa09742233bd6e58
Abdel Bailoul <ab.bailoul@gmail.com>
2023-11-04 08:53:32 +0100

    feat: added a data grid theme

---
+++ src/pages/Dashboard/theme/dataGridTheme.js
@@ -0,0 +1,11 @@
+import { frFR } from '@mui/x-data-grid';
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: { main: '#095262b0' },
+  },
+  frFR,
+});
+
+export default theme;
