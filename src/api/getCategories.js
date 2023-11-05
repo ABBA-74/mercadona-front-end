4dfef3aab1340ed5038a93f45e61f67e48c3de59
Abdel Bailoul <ab.bailoul@gmail.com>
2023-11-05 16:26:40 +0100

    feat: updated categories fetch function to support pagination

--- src/api/getCategories.js
+++ src/api/getCategories.js
@@ -1,12 +1,24 @@
 import axios from './axios';
 import { CATEGORIES_ENDPOINT } from './apiConfig';
 
-export const getCategories = async () => {
+/**
+ * Fetch and return categories data
+ * @param {number} [currentPage=null] - The current page number for pagination.
+ * @returns {{categories: Array, totalItems: Number}}
+ */
+export const getCategories = async (currentPage = null) => {
+  let url = '';
   try {
-    const responses = await axios.get(CATEGORIES_ENDPOINT);
+    if (currentPage) {
+      url = CATEGORIES_ENDPOINT + `/dashboard?page=${currentPage}`;
+    } else {
+      url = CATEGORIES_ENDPOINT;
+    }
+    const responses = await axios.get(url);
     const categories = responses.data['hydra:member'];
+    const totalItems = responses.data['hydra:totalItems'];
 
-    return { categories };
+    return { categories, totalItems };
   } catch (err) {
     console.error(err);
     throw err;
