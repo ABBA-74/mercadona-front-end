7318550c40ea34ab7ddef481027b3112a79079d2
Abdel Bailoul <ab.bailoul@gmail.com>
2023-11-07 17:30:50 +0100

    feat: updated crud notification provider file to handle clearTimeout

--- src/contexts/CrudNotificationProvider.jsx
+++ src/contexts/CrudNotificationProvider.jsx
@@ -1,17 +1,35 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const CrudNotificationContext = createContext({});
 
 export const CrudNotificationProvider = ({ children }) => {
   const [notification, setNotification] = useState(null);
+  const [timeoutId, setTimeoutId] = useState(null);
+
+  const clearNotificationTimeout = () => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      setTimeoutId(null);
+    }
+  };
 
   const showNotification = (type, message) => {
+    clearNotificationTimeout();
     setNotification({ type, message });
-    setTimeout(() => {
+
+    const id = setTimeout(() => {
       setNotification(null);
-    }, 5000);
+    }, 3000);
+    setTimeoutId(id);
   };
+
+  // when component will unmount => cleanup setTimeout
+  useEffect(() => {
+    return () => {
+      clearNotificationTimeout();
+    };
+  }, [timeoutId]);
 
   return (
     <CrudNotificationContext.Provider
