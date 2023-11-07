7ebb34c3bf4e5d1e167809cd6eace1fb6fcd860a
Abdel Bailoul <ab.bailoul@gmail.com>
2023-11-07 18:18:37 +0100

    feat: updated BtnsActionsCrud file to handle delete crud action for all items

--- src/pages/Dashboard/BtnsActionsCrud/BtnsActionsCrud.jsx
+++ src/pages/Dashboard/BtnsActionsCrud/BtnsActionsCrud.jsx
@@ -1,42 +1,96 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import CircularProgress from '@mui/material/CircularProgress';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import useCrudNotification from '../../../hooks/useCrudNotification';
+import { deleteItem } from '../../../api/deleteItem';
+import { scrollTo } from '../../../utils/scrollTo';
 import './BtnsActionsCrud.scss';
 
-const BtnsActionsCrud = ({ params }) => {
-  const handleEdit = () => {
-    console.log('Edit');
-    console.log(params.row['@id']);
+const BtnsActionsCrud = ({
+  params,
+  rowId,
+  setPage,
+  setRefreshDataGrid,
+  hideDeleteBtn,
+}) => {
+  const { showNotification } = useCrudNotification();
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleRefreshDataGrid = () => {
+    setPage(1);
+    setRefreshDataGrid(true);
+    scrollTo(0, 0);
   };
-  const handleDelete = () => {
-    console.log('Delete');
-    console.log(params.row['@id']);
+
+  const handleEditItem = () => {
+    showNotification('info', 'Opération réussie !');
+    handleRefreshDataGrid();
   };
+
+  const handleDeleteItem = async () => {
+    const itemUrl = params.row['@id'];
+    try {
+      setIsLoading(true);
+      const response = await deleteItem(itemUrl);
+      if (response.status === 204) {
+        handleRefreshDataGrid();
+      }
+      showNotification('info', response.message);
+    } catch (err) {
+      console.error('Erreur lors de la récupération des données', err);
+      showNotification(
+        'error',
+        "La suppression de l'élément a échoué. Veuillez réessayer."
+      );
+      if (err.response && err.response.data.code === 401) {
+        navigate('/login', { replace: true });
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       <button
         className='btn btn-primary btn-actions-crud btn-edit'
-        onClick={handleEdit}
+        disabled={isLoading}
+        onClick={handleEditItem}
       >
         <EditIcon className='icon-btn-actions' />
       </button>
-      <button
-        className='btn btn-danger btn-actions-crud btn-delete'
-        onClick={handleDelete}
-      >
-        <DeleteIcon className='icon-btn-actions' />
-      </button>
+
+      {!hideDeleteBtn && (
+        <button
+          className='btn btn-danger btn-actions-crud btn-delete'
+          disabled={isLoading}
+          onClick={handleDeleteItem}
+        >
+          {isLoading && params.id === rowId ? (
+            <CircularProgress color='inherit' size={20} />
+          ) : (
+            <DeleteIcon className='icon-btn-actions' />
+          )}
+        </button>
+      )}
     </>
   );
 };
 
 BtnsActionsCrud.propTypes = {
-  params: PropTypes.shape({
-    row: PropTypes.shape({
-      '@id': PropTypes.string.isRequired,
-      // others
-    }).isRequired,
-    id: PropTypes.number.isRequired,
-  }).isRequired,
+  params: PropTypes.object.isRequired,
+  rowId: PropTypes.number,
+  setPage: PropTypes.func.isRequired,
+  setRefreshDataGrid: PropTypes.func.isRequired,
+  hideDeleteBtn: PropTypes.bool,
 };
+
+BtnsActionsCrud.defaultProps = {
+  hideDeleteBtn: false,
+};
+
 export default BtnsActionsCrud;
